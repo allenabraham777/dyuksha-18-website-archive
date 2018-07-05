f52d5f479127be3c6a886b7ede7cc7edb9cf3e5f
Paul P Joby <paulpjoby@gmail.com>
2018-07-05 12:42:34 +0530

    Particle Js Added
    Menu Edit

---
+++ js/particle_exe.js
@@ -0,0 +1,62 @@
+/* ---- particles.js config ---- */
+
+particlesJS("cover", {
+  "particles": {
+    "number": {
+      "value": 200
+    },
+    "color": {
+      "value": "#f1f1f1"
+    },
+    "shape": {
+     "type": "circle",
+    },
+    "size": {
+      "value": 10,
+      "random": true
+    },
+    "line_linked": {
+      "enable": false
+    },
+    "move": {
+      "enable": true,
+      "speed": 2,
+      "direction": "bottom",
+      "straight": false
+    }
+  },
+  "interactivity": {
+    "detect_on": "canvas",
+    "events": {
+      "onhover": {
+        "enable": false
+      }
+    },
+    "modes": {
+      "push": {
+        "particles_nb": 12
+      }
+    }
+  }
+});
+
+
+/* ---- stats.js config ---- */
+
+var count_particles, stats, update;
+stats = new Stats;
+stats.setMode(0);
+stats.domElement.style.position = 'absolute';
+stats.domElement.style.left = '0px';
+stats.domElement.style.top = '0px';
+document.body.appendChild(stats.domElement);
+count_particles = document.querySelector('.js-count-particles');
+update = function() {
+  stats.begin();
+  stats.end();
+  if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) {
+    count_particles.innerText = window.pJSDom[0].pJS.particles.array.length;
+  }
+  requestAnimationFrame(update);
+};
+requestAnimationFrame(update);
