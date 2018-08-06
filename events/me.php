4f4d57acb8ec1fbd714d966707be0cee3c3dd0d3
Ajesh Krishnan <kmajeshrishnan@gmail.com>
2018-08-06 19:01:26 +0530

    Event images added! popup removed

--- events/me.php
+++ events/me.php
@@ -64,50 +64,6 @@
 <body onload="">
 
 
-
-<div class="dialog-container">
-        <div class="dialog">
-            <button class="dialog-close-button" onclick="openOrCloseDialog();"><i class="fas fa-times"></i></button>
-            <div class="dialog-radio-container-parent">
-                <label class="dialog-radio-container">All
-                        <input type="radio" value="6" name="dialog-radio" checked="checked" id="all" class="dialog-radio">
-                        <span class="dialog-radio-checkmark"></span>
-                    </label>
-                    
-                    <label class="dialog-radio-container">Computer Science
-                        <input type="radio" value="2" name="dialog-radio" id="cse" class="dialog-radio">
-                        <span class="dialog-radio-checkmark"></span>
-                    </label>
-                    
-                    <label class="dialog-radio-container">Mechanical
-                        <input type="radio" value="3" name="dialog-radio" id="me" class="dialog-radio">
-                        <span class="dialog-radio-checkmark"></span>
-                    </label>
-                    
-                    <label class="dialog-radio-container">Electrical
-                        <input type="radio" value="5" name="dialog-radio" id="eee" class="dialog-radio">
-                        <span class="dialog-radio-checkmark"></span>
-                    </label>
-                    
-                    <label class="dialog-radio-container">Electronics
-                        <input type="radio" value="4" name="dialog-radio" id="ece" class="dialog-radio">
-                        <span class="dialog-radio-checkmark"></span>
-                    </label>
-                    
-                    <label class="dialog-radio-container">Civil
-                        <input type="radio" value="0" name="dialog-radio" id="ce" class="dialog-radio">
-                        <span class="dialog-radio-checkmark"></span>
-                    </label>
-                    
-                    <label class="dialog-radio-container">Instrumentation Control
-                        <input type="radio" value="1" name="dialog-radio" id="ic" class="dialog-radio">
-                        <span class="dialog-radio-checkmark"></span>
-                    </label>
-                    <button class="dialog-filter-button" onclick="s();">Filter</button>
-            </div>      
-        </div>
-    </div>
-
 <?php
     // Menu as a Include File
     include("menu.php");
@@ -124,7 +80,7 @@
             while($row=mysqli_fetch_array($res)){
        
                 echo "<div class='events-card-1 {$row[0]}'>";
-                echo "<img src=\"https://s3-us-west-2.amazonaws.com/s.cdpn.io/331810/sample68.jpg\" style=\"width:100%; margin-bottom:0px;\">";
+                echo "<img src=\"images/event_img/{$row[0]}.jpg\" style=\"width:100%; margin-bottom:0px;\">";
                 echo "<h4>{$row[1]}</h4>";
                 echo "<p>{$row[3]} <br/><br/>";
                 echo "<span><i class=\"far fa-clock\"></i> Day: {$row[2]}</span>";
@@ -153,103 +109,6 @@
            document.getElementById("user_menu").classList.add("menu-collapse");
            document.getElementById("user_menu").classList.remove("menu-open");
        }
-    }
-
-    var  show = false;
-    function openOrCloseDialog(){
-        if(show){
-        document.getElementsByClassName("dialog-container")[0].style.display="none";
-        show=false;
-        }
-        else{
-        document.getElementsByClassName("dialog-container")[0].style.display="block";
-        show=true;
-        }
-    }
-     /*------------------------------------ Filter Method --------------------------------------------
-    | 0 ->CE 1->IC 2->CS 3->ME 4->EC 5->EE OTHER -> ALL
-    ------------------------------------------------------------------------------------------------*/
-    function filter(cat){
-        var x = parseInt(cat);
-        switch(x){
-            case 0:
-                var len = all_events.length;
-                for(var i=0;i<len;i++){
-                    document.getElementsByClassName(all_events[i])[0].style.display="none";
-                }
-                len = ce_events.length;
-                for(var i=0;i<len;i++){
-                    document.getElementsByClassName(ce_events[i])[0].style.display="inline-block";
-                }
-                break;
-            case 1:
-                var len = all_events.length;
-                for(var i=0;i<len;i++){
-                    document.getElementsByClassName(all_events[i])[0].style.display="none";
-                }
-                len = ic_events.length;
-                for(var i=0;i<len;i++){
-                    document.getElementsByClassName(ic_events[i])[0].style.display="inline-block";
-                }
-                break;
-            case 2:
-            var len = all_events.length;
-                for(var i=0;i<len;i++){
-                    document.getElementsByClassName(all_events[i])[0].style.display="none";
-                }
-                len = cs_events.length;
-                for(var i=0;i<len;i++){
-                    document.getElementsByClassName(cs_events[i])[0].style.display="inline-block";
-                }
-                break;
-            case 3:
-            var len = all_events.length;
-                for(var i=0;i<len;i++){
-                    document.getElementsByClassName(all_events[i])[0].style.display="none";
-                }
-                len = me_events.length;
-                for(var i=0;i<len;i++){
-                    document.getElementsByClassName(me_events[i])[0].style.display="inline-block";
-                }
-                break;
-            case 4:
-            var len = all_events.length;
-                for(var i=0;i<len;i++){
-                    document.getElementsByClassName(all_events[i])[0].style.display="none";
-                }
-                len = ec_events.length;
-                for(var i=0;i<len;i++){
-                    document.getElementsByClassName(ec_events[i])[0].style.display="inline-block";
-                }
-                break;
-            case 5:
-            var len = all_events.length;
-                for(var i=0;i<len;i++){
-                    document.getElementsByClassName(all_events[i])[0].style.display="none";
-                }
-                len = ee_events.length;
-                for(var i=0;i<len;i++){
-                    document.getElementsByClassName(ee_events[i])[0].style.display="inline-block";
-                }
-                break;
-            default:
-            var len = all_events.length;
-                for(var i=0;i<len;i++){
-                    document.getElementsByClassName(all_events[i])[0].style.display="inline-block";
-                }
-                
-
-        }
-    }
-
-    function s(){
-        var x = document.getElementsByName("dialog-radio");
-        for(var i=0;i<x.length;i++){
-            if(x[i].checked){
-            filter(x[i].value);
-            }
-        }
-        openOrCloseDialog();
     }
 </script>
 </body>
