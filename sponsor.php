ce245d8b3a9abe07a46eb2377a0337b4d5832d53
ALLEN K ABRAHAM <allenabraham777@gmail.com>
2018-08-07 23:11:00 +0530

    Sponsor page added

--- sponsor.php
+++ sponsor.php
@@ -6,41 +6,6 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
     <link rel="stylesheet" href="css/sponsor.css">
-    <style>
-        body{
-            margin: 0;
-        }
-        .ad_row{
-            text-align: center;
-            position: relative;
-            padding: 20px;
-            padding-right: 50px;
-            padding-left: 50px;
-
-        }
-        .brand{
-            width: 200px;
-            height: 200px;
-            margin-bottom: 20px;
-            margin-right: 20px;
-            display: inline-block;
-        }
-        .brand:nth-last-child(n){
-            margin-right: 0px;
-        }
-        .brand img{
-            width: 100%;
-        }
-        h1{
-            text-align: center;
-        }
-        @media screen and (max-width: 640px){
-            .ad_row{
-                padding-right: 20px;
-                padding-left: 20px;
-            }
-        }
-    </style>
 </head>
 <body>
     <h1>SPONSORS</h1>
