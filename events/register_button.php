2a8796b20c1b17b95e31386c4393b745e6084992
ALLEN K ABRAHAM <allenabraham777@gmail.com>
2018-08-08 00:21:47 +0530

    Register Button Optimisation

--- events/register_button.php
+++ events/register_button.php
@@ -1,4 +1,4 @@
-<button style="position: fixed; z-index: 100; bottom: 10px; right: 10px; height: 50px; font-size: 20px; border:none; border-radius: 15px; padding-right:10px; padding-left:10px; cursor: pointer;">
+<button onclick='location.href = "../registeration.php"' style="position: fixed; z-index: 100; bottom: 10px; right: 10px; height: 50px; font-size: 20px; border:none; border-radius: 15px; padding-right:10px; padding-left:10px; cursor: pointer;">
     <i class="far fa-bookmark"></i>
     <span>Register</span>
 </button>
