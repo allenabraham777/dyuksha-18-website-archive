19618af3f49b50a5800f56a0a2b93ace4dd4ea30
Paul P Joby <paulpjoby@gmail.com>
2018-08-09 00:37:33 +0530

    Created Details.js
    For displaying read and display rules

---
+++ js/details.js
@@ -0,0 +1,7 @@
+/*
+This file is created for displaying the rules and regulation of the Different Events
+*/
+function readMore(itemId){
+    link = "http://www.dyuksha.org/readmore?i="+itemId;
+    location.href=link;
+}
