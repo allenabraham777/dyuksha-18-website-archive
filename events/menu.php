aa0d274f806d88c6f17a89a4615af5d523f98e43
ALLEN K ABRAHAM <allenabraham777@gmail.com>
2018-08-09 12:11:07 +0530

    Some optimisation

--- events/menu.php
+++ events/menu.php
@@ -1,11 +1,13 @@
+
+
 <header <?php if((strpos($_SERVER['REQUEST_URI'], 'cs') || strpos($_SERVER['REQUEST_URI'], 'ic') || strpos($_SERVER['REQUEST_URI'], 'ce') || strpos($_SERVER['REQUEST_URI'], 'me') || strpos($_SERVER['REQUEST_URI'], 'ec') || strpos($_SERVER['REQUEST_URI'], 'ee') || strpos($_SERVER['REQUEST_URI'], 'general') )== false){echo 'style="width: 102%;"';}?>>
     <a href="../index"><img class="logo" src="images/navbar_logo.png"></a>
-    <span class="user_id" id="user_id" onclick="open_menu()">
+    <span class="user_id" id="user_id" onclick="open_menu()" <?php if((strpos($_SERVER['REQUEST_URI'], 'cs') || strpos($_SERVER['REQUEST_URI'], 'ic') || strpos($_SERVER['REQUEST_URI'], 'ce') || strpos($_SERVER['REQUEST_URI'], 'me') || strpos($_SERVER['REQUEST_URI'], 'ec') || strpos($_SERVER['REQUEST_URI'], 'ee') || strpos($_SERVER['REQUEST_URI'], 'general') )== false){echo 'style="margin-right:20px;';} echo 'font-family:verdana;"';?>>
         <i class="fas fa-user"></i> <?php echo $name ?> <i class="fas fa-sort-down"></i>
         <!-- PHP SCRIPT -->
     </span>
     <button class="menu-button" onclick="open_menu()" style="line-height:100%;"><i class="fas fa-bars"></i></button>
-    <div class="user_menu menu-collapse" id="user_menu">
+    <div class="user_menu menu-collapse" id="user_menu" <?php if((strpos($_SERVER['REQUEST_URI'], 'cs') || strpos($_SERVER['REQUEST_URI'], 'ic') || strpos($_SERVER['REQUEST_URI'], 'ce') || strpos($_SERVER['REQUEST_URI'], 'me') || strpos($_SERVER['REQUEST_URI'], 'ec') || strpos($_SERVER['REQUEST_URI'], 'ee') || strpos($_SERVER['REQUEST_URI'], 'general') )== false){echo 'style="margin-right:20px;';} echo '"';?>>
         <ul>
             <li><a href="../index"><i class="fas fa-home"></i>&nbsp;&nbsp;Home</a></li>
             <li><a href="../profile"><i class="fas fa-shopping-cart"></i>&nbsp;&nbsp;Profile</a></li>
