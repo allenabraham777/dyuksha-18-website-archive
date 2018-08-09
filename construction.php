bc98e2eb92899ed50fed867cc0aad56a8fe9e73f
ALLEN K ABRAHAM <allenabraham777@gmail.com>
2018-08-09 09:39:49 +0530

    construction page

---
+++ construction.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Under Construction</title>
+    <script defer src="https://use.fontawesome.com/releases/v5.1.0/js/all.js" integrity="sha384-3LK/3kTpDE/Pkp8gTNp2gR/2gOiwQ6QaO7Td0zV76UFJVhqLl4Vl3KL1We6q6wR9" crossorigin="anonymous"></script>
+    <link rel="icon" href="images/logo.png" type="image/png" />
+    <style>
+        body{
+            font-family: arial;
+            margin:0;
+            text-align: center;
+            width: 100vw;
+        }
+        img{
+            margin-top:50px;
+            height: 100px;
+        }
+        .h404{
+            color: #347cb4;
+            padding-top: 50px;
+            padding-bottom: 30px;
+            width: 100%;
+            font-size: 50px;
+            font-weight: bold;
+        }
+        .sh404{
+            color: #fc647c;
+            width: 100%;
+            font-weight: bold;
+            font-size: 30px;
+        }
+    </style>
+</head>
+<body>
+    <img src="images/logo2.png">
+    <div class="h404"><i class="fas fa-wrench" style="color:#333;"></i><br>Page Under Construction</div>
+    <div class="sh404">Will to available soon....</div>
+</body>
+</html>
