15fe8e9839692f00a2f67373a501570b12806a3a
ALLEN K ABRAHAM <allenabraham777@gmail.com>
2018-08-18 23:40:10 +0530

    Home Button and switch between pages in signup and login

--- create.php
+++ create.php
@@ -2,7 +2,7 @@
 session_start();
  //session_destroy();
  if(isset($_SESSION["user"])){
-     header("Location:profile.php");
+     header("Location:profile");
      exit();
  }
  ?>
@@ -14,17 +14,52 @@ session_start();
     <title>Create Account</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" type="text/css" media="screen" href="css/create.css" />
+    <link rel="stylesheet" type="text/css" media="screen" href="css/menuitems.css" />
     <link rel="stylesheet prefetch" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+    <script defer src="https://use.fontawesome.com/releases/v5.1.1/js/all.js" integrity="sha384-BtvRZcyfv4r0x/phJt9Y9HhnN5ur1Z+kZbKVgzVBAlQZX4jvAuImlIz+bG7TS00a" crossorigin="anonymous"></script>
     <script src="js/jquery.js"></script>
     <script src="js/jquery.validate.js"></script>
     <script src="js/validate.js"></script>
     <script src="js/sweetalert.js"></script>
+    <script>
+        function open_menu(){
+        if(document.getElementById("user_menu").classList.contains("menu-collapse")){
+           document.getElementById("user_menu").classList.add("menu-open");
+           document.getElementById("user_menu").classList.remove("menu-collapse");
+        }
+        else{
+           document.getElementById("user_menu").classList.add("menu-collapse");
+           document.getElementById("user_menu").classList.remove("menu-open");
+       }
+    }
+    </script>
 </head>
 <body>
-    <div class="bg-create-page" align="center">
+
+<header>
+    <a href="index"><img class="logo" src="images/navbar_logo.png"></a>
+    <span class="user_id" id="user_id" onclick="open_menu()">
+        <i class="fas fa-user"></i> Guest <i class="fas fa-sort-down"></i>
+        <!-- PHP SCRIPT -->
+    </span>
+    <button class="menu-button" onclick="open_menu()"><i class="fas fa-bars"></i></button>
+    <div class="user_menu menu-collapse" id="user_menu">
+        <ul>
+            <li><a href="index"><i class="fas fa-home"></i>&nbsp;&nbsp;Home</a></li>
+            <li><a href="events"><i class="fas fa-calendar-alt"></i>&nbsp;&nbsp;Events</a></li>
+            <li><a href="workshop"><i class="fas fa-briefcase"></i>&nbsp;&nbsp;Workshops</a></li>
+            <li><a href="informals"><i class="fas fa-gamepad"></i>&nbsp;&nbsp;Informals</a></li>
+        </ul>
+    </div>
+</header>
+
+
+
+
+    <div class="bg-create-page" align="center" style="margin-top:60px;">
          <div class="create-box" align="center">
             <form action="create.php" name="regform" method="post" algin="center" class="create-form" id="create-form-id">
-                <img src="images/tag.png" width="60px" class="register-img">
+                <a href="login"><img src="images/tag.png" width="60px" class="register-img"></a>
                 
                 <div class="custom-input">
                         <input type="text" class="inputbox" name="name" id="field1" placeholder="Full Name" required/><br/>
@@ -56,6 +91,8 @@ session_start();
                         <img src="images/password.png" width="20px" />
                 </div>
                 <input type="submit" class="button-yellow" value="Create Account" onclick="checkPasswordField();"/>
+                or <a href="login">Login</a> to Dyuksha          
+
             </form>
     
     <?php
--- login.php
+++ login.php
@@ -37,13 +37,33 @@
     <link rel="icon" href="images/logo.png" type="image/png" />
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" type="text/css" media="screen" href="css/create.css" />
+    <link rel="stylesheet" type="text/css" media="screen" href="css/menuitems.css" />
     <link rel="shortcut icon" type="image/png" media="screen" href="images/logo.png" />
+    <script defer src="https://use.fontawesome.com/releases/v5.1.1/js/all.js" integrity="sha384-BtvRZcyfv4r0x/phJt9Y9HhnN5ur1Z+kZbKVgzVBAlQZX4jvAuImlIz+bG7TS00a" crossorigin="anonymous"></script>
     <script src="js/create.js"></script>
 </head>
 <body>
-    <div class="bg-create-page" align="center">
+    <header>
+        <a href="index"><img class="logo" src="images/navbar_logo.png"></a>
+        <span class="user_id" id="user_id" onclick="open_menu()">
+            <i class="fas fa-user"></i> Guest <i class="fas fa-sort-down"></i>
+            <!-- PHP SCRIPT -->
+        </span>
+        <button class="menu-button" onclick="open_menu()"><i class="fas fa-bars"></i></button>
+        <div class="user_menu menu-collapse" id="user_menu">
+            <ul>
+                <li><a href="index"><i class="fas fa-home"></i>&nbsp;&nbsp;Home</a></li>
+                <li><a href="events"><i class="fas fa-calendar-alt"></i>&nbsp;&nbsp;Events</a></li>
+                <li><a href="workshop"><i class="fas fa-briefcase"></i>&nbsp;&nbsp;Workshops</a></li>
+                <li><a href="informals"><i class="fas fa-gamepad"></i>&nbsp;&nbsp;Informals</a></li>
+            </ul>
+        </div>
+    </header>
+    <div class="bg-create-page" align="center" style="margin-top:60px;">
      <?php
-     if(isset($_POST["email"]) && isset($_POST["password"]))
+
+
+    if(isset($_POST["email"]) && isset($_POST["password"]))
      {
          if(isset($active)){
             if(strcmp($active,"YES") != 0){
@@ -72,14 +92,26 @@
      ?>   
     <div class="login-box" align="center">
             <form action="login" method="post" algin="center" class="create-form">
-            <img src="images/logo.png" class="login-image" width="58px"/>
+            <a href="home"><img src="images/logo.png" class="login-image" width="58px"/></a>
                 <input type="email" class="inputbox" name="email" placeholder="Email Id" required/><br/>
                 <input type="password" class="inputbox" name="password" placeholder="Password" required/><br/>
                 <input type="submit" class="button-tomato" value="Login" /></br>
                 <a href="/reset" class="">Forgot Password</a>
                 <a href="create.php" class="">Create an Account</a>
-            </form>          
+            </form>
     </div>
     </div>
+    <script type="text/javascript">
+    function open_menu(){
+        if(document.getElementById("user_menu").classList.contains("menu-collapse")){
+           document.getElementById("user_menu").classList.add("menu-open");
+           document.getElementById("user_menu").classList.remove("menu-collapse");
+        }
+        else{
+           document.getElementById("user_menu").classList.add("menu-collapse");
+           document.getElementById("user_menu").classList.remove("menu-open");
+       }
+    }
+</script>
 </body>
 </html>
